7e1d22db04531a998a5ac31764d40fb26203234d
max-wacquier <max.wacquier@gmail.com>
2024-06-20 16:43:54 +0200

    full translations in loginForm

--- src/components/templates/loginForm/LoginForm.jsx
+++ src/components/templates/loginForm/LoginForm.jsx
@@ -12,17 +12,17 @@ const LoginForm = () => {
   return (
     <>
       <div className="form-register-container">
-        <p className="form-title">Login</p>
+        <p className="form-title">{t('login.title')}</p>
         <Form id="login-form" className="form-register">
           <Input type="text" id="pseudo" name="pseudo" label={t('login.username')} />
           <Input type="password" id="pwd" name="pwd" label={t('login.password')} />
-          <SubmitButton onClick="submit">LOGIN</SubmitButton>
+          <SubmitButton onClick="submit">{t('login.submit')}</SubmitButton>
         </Form>
         <div className="logo-container">
           <img src={forgeLogo} alt="Logo" />
         </div>
         <p className="form-register-link">
-          Vous n'avez pas de compte<a>S'inscre</a>
+          {t('login.anyAccount')} <a>{t('login.signUp')}</a>
         </p>
       </div>
     </>
