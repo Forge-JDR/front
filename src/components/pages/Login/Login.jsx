f9ecd42bc36d9b3177438d045e859dd3e057d3d2
max-wacquier <max.wacquier@gmail.com>
2024-07-04 11:24:48 +0200

    conflicts reselved

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import "./login.css";
-import LoginForm from "../../templates/loginForm/LoginForm";
 import Hexagon from "../../UI/atoms/hexagon/hexagon";
 import HexagonBackground from "../../UI/molecules/hexagonsBackground/hexagonsBackground";
+import SubmitButton from "../../UI/molecules/submitButton/submitButton";
+import Form from "../../UI/organisms/Form";
+import FieldForm from "../../UI/molecules/FieldForm/FieldForm";
+import { useTranslation } from "react-i18next";
+import forgeLogo from "../../../assets/logo_complet.svg";
 
 const Login = () => {
-  return <LoginForm></LoginForm>;
+  const { t } = useTranslation();
+  return (
+    <>
+      <div className="form-register-container">
+        <p className="form-title">Login</p>
+        <Form id="login-form" className="form-register">
+          <FieldForm
+            label={t("login.username") + " *"}
+            name="pseudo"
+            required={true}
+          />
+          <FieldForm
+            type="password"
+            label={t("login.password") + " *"}
+            name="password"
+            required={true}
+          />
+          <SubmitButton onClick="submit">{t("login.submit")}</SubmitButton>
+        </Form>
+        <div className="logo-container">
+          <img src={forgeLogo} alt="Logo" />
+        </div>
+        <p className="form-register-link">
+          {t("login.anyAccount")} <a href="/signup">{t("login.signUp")}</a>
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Login;
